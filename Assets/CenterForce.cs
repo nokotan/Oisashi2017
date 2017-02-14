f48a7c2284f3c90a58fac2eae35a4456d2a55191
nokotan <kyoshizaki@jcom.home.ne.jp>
2017-02-14 10:46:12 +0900

    リポジトリの初期化

---
+++ Assets/CenterForce.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CenterForce : MonoBehaviour {
+
+    private Rigidbody Rigit;
+
+    public Vector3 CenterPosition;
+    public float Magnitude;
+
+    // Use this for initialization
+    void Start () {
+        Rigit = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var TransForm = GetComponent<Transform>();
+        var CenterDirection = CenterPosition - transform.position;
+
+        if (CenterDirection.sqrMagnitude > 0.001f) {
+            Rigit.AddForce(CenterDirection.normalized * Magnitude);
+        }
+	}
+}
---
+++ Assets/Frickable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Frickable : MonoBehaviour {
+
+    private Vector3? PressStartPos;
+    private Rigidbody Rigit;
+
+	// Use this for initialization
+	void Start () {
+        Rigit = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButton(0)) {
+            if (PressStartPos == null) {
+                PressStartPos = Input.mousePosition;
+            }
+        } else {
+            if (PressStartPos != null) {
+                var Difference = Input.mousePosition - PressStartPos.Value;
+                Difference = Quaternion.Euler(90.0f, 0.0f, 0.0f) * Difference;
+
+                Rigit.AddForce(Difference);
+                PressStartPos = null;
+            }
+        }
+	}
+}
